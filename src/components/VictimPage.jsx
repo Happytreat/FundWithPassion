72ecb542f38522d7f36a15ce67c24b1cecbfe640
suriruhani <suriruhani@gmail.com>
2019-07-06 19:16:07 +0800

    implement form

--- src/components/VictimPage.jsx
+++ src/components/VictimPage.jsx
@@ -18,15 +18,50 @@ const data = [
 ];
 
 class VictimPage extends Component {
-    constructor() {
-        super();
-        this.state = { showForm: false }
+
+
+    constructor () {
+        super()
+        this.state = {
+            showForm: false ,
+            formControls: {
+                item: {
+                    value: 'enter resource needed'
+                },
+                name: {
+                    value: 'your name'
+                },
+                address: {
+                    value: 'place to deliver'
+                },
+                quantity: {
+                    value: 1
+                }
+            }
+        }
+
     }
 
     _showForm = (bool) => {
         this.setState({
-            showForm: bool
+            showForm: bool,
         });
+    }
+
+    changeHandler = event => {
+
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            formControls: {
+                [name]: value
+            }
+        });
+
+    }
+
+    handleSubmit(event) {
     }
 
     render() {
@@ -40,7 +75,7 @@ class VictimPage extends Component {
                     renderItem={item => (
                         <List.Item>
                             <Card title={item.title} >
-                                <Button onClick={this._showForm.bind(null, true)}>
+                                <Button onClick={this._showForm.bind(null, !this.state.showForm)}>
                                     Click to get help
                                 </Button>
                             </Card>
@@ -51,7 +86,33 @@ class VictimPage extends Component {
 
                 {
                     this.state.showForm && (<div>
-                        <h3> Fill in the form below </h3>
+                        <h3> Fill in the form below: </h3>
+                        <form onSubmit={this.handleSubmit}>
+                            <p>
+                            <label> Name:
+                            <input type="text" name="name" value = {this.state.name} onChange={this.changeHandler} />
+                            </label>
+                            </p>
+                            <p>
+                            <label> Address:
+                                <input type="text" name="address" value = {this.state.address} onChange={this.changeHandler} />
+                            </label>
+                            </p>
+                            <p>
+                            <label> Item:
+                                <input type="text" name="item" value = {this.state.item} onChange={this.changeHandler} />
+                            </label>
+                            </p>
+                            <p>
+                            <label> Quantity:
+                                <input type="text" name="quantity" value = {this.state.quantity} onChange={this.changeHandler} />
+                            </label>
+                            </p>
+                            <p>
+                            <input type="submit" value="Submit" />
+                            </p>
+                        </form>
+
                     </div>)
                 }
             </div>
