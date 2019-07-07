72ac75915038444a3fcd0449359ac5241e092717
Happytreat <cowfats@gmail.com>
2019-07-07 09:41:28 +0800

    Add wallet feature and disable summon when not enough money

--- src/components/Game/Summon.jsx
+++ src/components/Game/Summon.jsx
@@ -1,23 +1,31 @@
 import React, { Component } from 'react'
 import { Image } from './Image'
+import { Button } from 'antd'
 
 class Summon extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      robots: []
+      robots: [],
+      amount: 50,
+      error: ''
     };
   }
 
   render() {
-    const { robots } = this.state;
+    const { robots, amount, error } = this.state;
 
     const handleClick = () => {
-      const hash = (Math.pow(10, 6)*Math.random()).toFixed(0);
-      // console.log(`https://{robohash.org/${hash}`);
-      const newR = robots;
-      newR[robots.length] = `https://robohash.org/${hash}`;
-      this.setState({robots: newR})
+      if (amount >= 10) {
+        const hash = (Math.pow(10, 6) * Math.random()).toFixed(0);
+        // console.log(`https://{robohash.org/${hash}`);
+        const newR = robots;
+        newR[robots.length] = `https://robohash.org/${hash}`;
+        const newA = amount - 10;
+        this.setState({robots: newR, amount: newA})
+      } else {
+        this.setState({ error: 'Not enough money to summon.' })
+      }
     };
     const items = [];
     robots.forEach(r => items.push(<Image link={r} />));
@@ -27,10 +35,19 @@ class Summon extends Component {
         <h1>
           Welcome to the summoning square.
         </h1>
-        <h4>
+        <h2>
           Summon your unique battle PassionRobos for only $10 which will be donated to a beneficiary of your choice.
-        </h4>
-        <button onClick={handleClick} type="primary">Summon your unique PassionRobos here!</button>
+        </h2>
+        <Button onClick={handleClick} type="primary">Summon your unique PassionRobos here!</Button>
+        <br />
+        <br />
+        <h3>
+          Your wallet has ${amount}
+        </h3>
+        <br />
+        {
+          <p style={{color: 'red'}}>{error}</p>
+        }
         <hr />
         {
           items
